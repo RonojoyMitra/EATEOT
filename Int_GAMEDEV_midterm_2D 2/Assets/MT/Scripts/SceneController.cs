b1ebe66acb5af1bdae75d1af01e0311c8b1cc16e
Michael Poltronieri Tang <mtang8264@gmail.com>
2021-02-21 15:33:39 -0500

    Created a simple SceneController
    This script is able to load a given scene by its name/path or its build index. It has an animator built into it that fades to black and then loads a loading scene. It then loads the target scene asynchronously allowing for a future loading screen.

---
+++ Int_GAMEDEV_midterm_2D 2/Assets/MT/Scripts/SceneController.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneController : MonoBehaviour
+{
+    static SceneController instance;
+
+    Animator animator;
+    Image blackout;
+
+    [SerializeField]
+    int loadScreenIndex;
+
+    void Awake()
+    {
+        if (instance != null) Destroy(gameObject);
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+        animator = GetComponent<Animator>();
+        blackout = GetComponentInChildren<Image>();
+    }
+
+    public static void LoadScene(int sceneBuildIndex)
+    {
+        instance.GoToScene(sceneBuildIndex);
+    }
+    public static void LoadScene(string sceneName)
+    {
+        instance.GoToScene(sceneName);
+    }
+
+    void GoToScene(int sceneBuildIndex)
+    {
+        StartCoroutine(GoToSceneCoroutine(sceneBuildIndex));
+    }
+    void GoToScene(string sceneName)
+    {
+        StartCoroutine(GoToSceneCoroutine(sceneName));
+    }
+
+    IEnumerator GoToSceneCoroutine(int sceneBuildIndex)
+    {
+        yield return new WaitForEndOfFrame();   // WaitForEndOfFrame to allow the coroutine to breath
+
+        // Fade out and wait for the opaque animation to be playing
+        animator.SetTrigger("Out");
+        yield return new WaitForEndOfFrame();
+        yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).IsName("Opaque"));
+
+        // Load the loading screen scene
+        SceneManager.LoadScene(loadScreenIndex);
+        yield return new WaitForEndOfFrame();
+
+        // Start loading the target scene
+        AsyncOperation l = SceneManager.LoadSceneAsync(sceneBuildIndex);
+        yield return new WaitForEndOfFrame();
+
+        // Wait for the scene load to be done
+        yield return new WaitUntil(() => l.isDone);
+
+        // Fade in
+        animator.SetTrigger("In");
+        yield return new WaitForEndOfFrame();
+        yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).IsName("Transparent"));
+    }
+    IEnumerator GoToSceneCoroutine(string sceneName)
+    {
+        yield return new WaitForEndOfFrame();   // WaitForEndOfFrame to allow the coroutine to breath
+
+        // Fade out and wait for the opaque animation to be playing
+        animator.SetTrigger("Out");
+        yield return new WaitForEndOfFrame();
+        yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).IsName("Opaque"));
+
+        // Load the loading screen scene
+        SceneManager.LoadScene(loadScreenIndex);
+        yield return new WaitForEndOfFrame();
+
+        // Start loading the target scene
+        AsyncOperation l = SceneManager.LoadSceneAsync(sceneName);
+        yield return new WaitForEndOfFrame();
+
+        // Wait for the scene load to be done
+        yield return new WaitUntil(() => l.isDone);
+
+        // Fade in
+        animator.SetTrigger("In");
+        yield return new WaitForEndOfFrame();
+        yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).IsName("Transparent"));
+    }
+}
