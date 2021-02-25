b0d7a6d1d787103afa8ede61ba9e74f2ff01101e
Michael Poltronieri Tang <mtang8264@gmail.com>
2021-02-25 12:53:33 -0500

    Updated spring platforms to be able to rotate slightly

--- Int_GAMEDEV_midterm_2D 2/Assets/MT/Scripts/PhysicsPlatform.cs
+++ Int_GAMEDEV_midterm_2D 2/Assets/MT/Scripts/PhysicsPlatform.cs
@@ -5,26 +5,10 @@ using UnityEngine;
 public class PhysicsPlatform : MonoBehaviour
 {
     [SerializeField]
-    float maxRotation;
+    DistanceJoint2D rotationLock;
 
-    void LateUpdate()
+    private void FixedUpdate()
     {
-        Vector3 e = transform.eulerAngles;
-        e.z = ClampAngle(e.z, -maxRotation, maxRotation);
-        transform.eulerAngles = e;
-    }
-
-    float ClampAngle(float angle, float min, float max)
-    {
-        while (min < -180f) min += 360f;
-        while (max < -180f) max += 360f;
-        while (angle < -180f) angle += 360f;
-        while (min > 180f) min -= 360f;
-        while (max > 180f) max -= 360f;
-        while (angle > 180f) angle -= 360f;
-
-        if (angle > max) return max;
-        if (angle < min) return min;
-        return angle;
+        rotationLock.connectedAnchor = new Vector2(transform.position.x - 1.5f, transform.position.y);
     }
 }
