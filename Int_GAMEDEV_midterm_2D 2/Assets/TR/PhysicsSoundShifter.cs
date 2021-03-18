3074ae66f7d8b11ccb4c886736e99d36eb469af6
TR <atheologidesrodriguez@gmail.com>
2021-03-18 06:26:35 -0400

    Tuned the phyics sound shifter.

--- Int_GAMEDEV_midterm_2D 2/Assets/TR/PhysicsSoundShifter.cs
+++ Int_GAMEDEV_midterm_2D 2/Assets/TR/PhysicsSoundShifter.cs
@@ -18,40 +18,36 @@ public class PhysicsSoundShifter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*
-        if (!stoodOn)
+        
+        if (!stoodOn && GetComponent<StudioGlobalParameterTrigger>().value > 0f)
         {
-            RuntimeManager.StudioSystem.setParameterByName("Physics Weirdness", GetComponent<StudioGlobalParameterTrigger>().value - 0.01f);
-            print(GetComponent<StudioGlobalParameterTrigger>().value);
+            GetComponent<StudioGlobalParameterTrigger>().value -= 0.001f;
+            GetComponent<StudioGlobalParameterTrigger>().TriggerParameters();
+            //RuntimeManager.StudioSystem.setParameterByName("Physics Weirdness", GetComponent<StudioGlobalParameterTrigger>().value - 0.01f);
+            //print(GetComponent<StudioGlobalParameterTrigger>().value);
         }
-        */
+        
     }
-    
+
     void OnCollisionStay2D(Collision2D other)
     {
         if (!stoodOn)
         {
             stoodOn = true;
         }
-        GetComponent<StudioGlobalParameterTrigger>().value += 0.01f;
+
+        GetComponent<StudioGlobalParameterTrigger>().value += 0.001f;
+        GetComponent<StudioGlobalParameterTrigger>().TriggerParameters();
         //RuntimeManager.StudioSystem.setParameterByName("Physics Weirdness", GetComponent<StudioGlobalParameterTrigger>().value + 0.01f);
-        print(GetComponent<StudioGlobalParameterTrigger>().value);
+        //print(GetComponent<StudioGlobalParameterTrigger>().value);
     }
-    /*
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        stoodOn = true;
-        GetComponent<StudioGlobalParameterTrigger>().value += 0.01f;
-        //RuntimeManager.StudioSystem.setParameterByName("Physics Weirdness", 1f);
-        print(GetComponent<StudioGlobalParameterTrigger>().value);
-    }
+
 
     void OnCollisionExit2D(Collision2D other) //when the player is no longer touching the
     {
         stoodOn = false;
         //GetComponent<StudioGlobalParameterTrigger>().value = 0f;
         //RuntimeManager.StudioSystem.setParameterByName("Physics Weirdness", 0f);
-        print(GetComponent<StudioGlobalParameterTrigger>().value);
+        //print(GetComponent<StudioGlobalParameterTrigger>().value);
     }
-    */
 }
