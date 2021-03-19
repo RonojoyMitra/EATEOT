09330c49a4cca30866bcfa8620d317f4fb1556dd
TR <atheologidesrodriguez@gmail.com>
2021-03-19 11:45:04 -0400

    Stitched stages and set up sound progression

--- Int_GAMEDEV_midterm_2D 2/Assets/TR/AudioEventTrigger.cs
+++ Int_GAMEDEV_midterm_2D 2/Assets/TR/AudioEventTrigger.cs
@@ -1,18 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioEventTrigger : MonoBehaviour
 {
+    private AudioManager myManager;
+
+    public float myTargetRed, myTargetGreen, myTargetBlue;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myManager = GameObject.Find("Audio Manager").GetComponent<AudioManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (myManager.targetRed < myTargetRed)
+        {
+            myManager.targetRed = myTargetRed;
+        }
+        if (myManager.targertGreen < myTargetGreen)
+        {
+            myManager.targertGreen = myTargetGreen;
+        }
+        if (myManager.targetBlue < myTargetBlue)
+        {
+            myManager.targetBlue = myTargetBlue;
+        }
     }
 }
--- Int_GAMEDEV_midterm_2D 2/Assets/TR/AudioManager.cs
+++ Int_GAMEDEV_midterm_2D 2/Assets/TR/AudioManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    
+    public float targetRed, targertGreen, targetBlue, currentRed, currentGreen, currentBlue;
     void Awake()
     {
         //DontDestroyOnLoad(gameObject); //Setting the audiomanager object to dontdestroyonload so we can choose to have sounds transition smoothly across scenes
@@ -19,6 +19,20 @@ public class AudioManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentRed < targetRed)
+        {
+            currentRed += 0.01f;
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Red Weirdness", currentRed);
+        }
+        if (currentGreen < targertGreen)
+        {
+            currentGreen += 0.01f;
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Green Weirdness", currentGreen);
+        }
+        if (currentBlue < targetBlue)
+        {
+            currentBlue += 0.01f;
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Blue Weirdness", currentBlue);
+        }
     }
 }
